85094fba748ab53f9296b51e861bed836d7613ea
Gabriel Checchi <checchigab@gmail.com>
2023-09-14 19:22:27 -0300

    Next.js

---
+++ components/layout.js
@@ -0,0 +1,14 @@
+
+
+export default function Layout() {
+    return (
+        <div className="navbar navbar-sticky">
+            <div className="navbar-start">
+                <a className="navbar-item">TODO List</a>
+            </div>
+            <div className="navbar-end">
+                <a className="navbar-item">Crear Tarea</a>
+            </div>
+        </div>
+    );
+}
