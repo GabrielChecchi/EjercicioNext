0d5de91fb22d08ebef6753bd01b72c50f570b08c
Gabriel Checchi <checchigab@gmail.com>
2023-09-14 20:16:21 -0300

    Create page

--- pages/create.js
+++ pages/create.js
@@ -1,26 +1,35 @@
+import { useState } from "react"
+
 export default function Create() {
+    const [nombre, setNombre] = useState("");
+
+    function handleOnNombreInput(e) {
+        setNombre(e.target.value);
+    }
+
+    function handleOnClick(e) {
+        
+    }
+
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <form method="POST">
-                <div className="mx-auto flex w-full max-w-sm flex-col gap-6">
-                    <div className="flex flex-col items-center">
-                        <h1 className="text-3xl font-semibold">Crear Tarea</h1>
+            <div className="mx-auto flex w-full max-w-sm flex-col gap-6">
+                <div className="flex flex-col items-center">
+                    <h1 className="text-3xl font-semibold">Crear Tarea</h1>
+                </div>
+                <div className="form-group">
+                    <div className="form-field">
+                        <label className="form-label">Nombre de la tarea</label>
+                        <input placeholder="Type here" type="text" className="input max-w-full" value={nombre} onInput={handleOnNombreInput} />
                     </div>
-                    <div className="form-group">
-                        <div className="form-field">
-                            <label className="form-label">Nombre de la tarea</label>
-                            <input placeholder="Type here" type="email" className="input max-w-full" />
-                        </div>
 
-                        <div className="form-field pt-5">
-                            <div className="form-control justify-between">
-                                <input type="submit" value="Crear Tarea" className="btn btn-primary w-full" />
-                            </div>
+                    <div className="form-field pt-5">
+                        <div className="form-control justify-between">
+                            <input type="submit" value="Crear Tarea" className="btn btn-primary w-full" onClick={handleOnClick} />
                         </div>
                     </div>
                 </div>
-            </form>
-
+            </div>
         </main>
     )
 }
